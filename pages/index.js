971de9e8cbed8cf75731d6d86ae8ac757b33b3ad
rakesh <rakesh.tagadghar@dltapps.co.uk>
2022-08-25 13:44:27 +0530

    inital commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import React, { useState } from "react";
+import Head from "next/head";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import CryptoJS from "crypto-js";
+import Typography from "@mui/material/Typography";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [value, setValue] = useState("");
+  const [d, setd] = useState("");
+  const handleDecrypt = () => {
+    const a = value && CryptoJS.AES.decrypt(value, "Zilo");
+    console.log(a);
+    const b = a && JSON.parse(a?.toString(CryptoJS.enc.Utf8));
+    setd(b);
+  };
   return (
     <div className={styles.container}>
       <Head>
@@ -12,58 +26,32 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <h1 className={styles.title}>Decode JWT</h1>
+            <Box>
+              <TextField
+                required
+                value={value}
+                id="outlined-required"
+                label="Encrypted key"
+                onChange={(e) => setValue(e.target.value)}
+              />
+            </Box>
+            <Box>
+              <Button variant="contained" onClick={() => handleDecrypt()}>
+                Decode
+              </Button>
+            </Box>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            {d && (
+              <Grid item xs>
+                <Typography>{d}</Typography>
+              </Grid>
+            )}
+          </Grid>
+        </Grid>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
